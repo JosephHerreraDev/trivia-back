760f1751490f26d468d7d80f87122d32a5e02125
Joseph Herrera <josephherreraleon@gmail.com>
2023-12-04 21:30:46 -0600

    Add socket.io for real-time communication

--- index.js
+++ index.js
@@ -3,6 +3,22 @@ const app = express();
 const port = 3000;
 const cors = require("cors");
 
+const io = require("socket.io")(4000, {
+  cors: {
+    origin: "*",
+  },
+});
+
+io.on("connection", (socket) => {
+  console.log("connected from" + socket.id);
+  socket.on("username", (username) => {
+    console.log(username);
+  });
+  socket.on("score", (score) => {
+    console.log(score);
+  });
+});
+
 const categoriesRouter = require("./routes/categories");
 const questionsRouter = require("./routes/questions");
 
